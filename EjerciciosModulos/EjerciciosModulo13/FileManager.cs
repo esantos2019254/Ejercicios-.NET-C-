ba48ea26707d1b08c0a88bbe19fe0d6edf283459
esantos2019254 <esantos-2019254@kinal.edu.gt>
2024-09-03 16:24:18 -0600

    Ejercicio 1 modulo 13 terminado

---
+++ EjerciciosModulos/EjerciciosModulo13/FileManager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Ejercicio_1
+{
+    class FileManager
+    {
+        public class ArchivoNoEncontradoException : Exception
+        {
+            public ArchivoNoEncontradoException(string mensaje) : base(mensaje)
+            {
+
+            }
+        }
+
+        public class ArchivoSiExisteException : Exception
+        {
+            public ArchivoSiExisteException(string mensaje) : base(mensaje)
+            {
+
+            }
+        }
+
+        public static void VerificarArchivo(string nombreArchivo)
+        {
+            if (!File.Exists(nombreArchivo))
+            {
+                throw new ArchivoNoEncontradoException("Archivo no encontrado");
+            }
+        }
+
+        public static void CrearArchivo(string nombreArchivo, string cadena)
+        {
+            if (File.Exists(nombreArchivo))
+            {
+                throw new ArchivoSiExisteException("El archivo ya existe.");
+            }
+            else
+            {
+                try
+                {
+                    using (FileStream fs = new FileStream(nombreArchivo, FileMode.Create))
+                    {
+                        byte[] bytes = Encoding.UTF8.GetBytes(cadena);
+                        fs.Write(bytes, 0, bytes.Length);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Ocurrió un error: {e.Message}");
+                }
+            }
+        }
+
+        public static void MostrarArchivo (string nombreArchivo)
+        {
+            if (!File.Exists(nombreArchivo))
+            {
+                throw new ArchivoNoEncontradoException("Archivo no encontrado");
+            }
+            else
+            {
+                try
+                {
+                    using (FileStream fs = new FileStream(nombreArchivo, FileMode.Open, FileAccess.Read))
+                    {
+                        byte[] buffer = new byte[fs.Length];
+
+                        int bytesRead = fs.Read(buffer, 0, buffer.Length);
+
+                        string contenido = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        Console.WriteLine("Contenido del archivo:");
+                        Console.WriteLine(contenido);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Ocurrió un error: {e.Message}");
+                }
+            }
+        }
+
+        public static void AgregarTexto(string nombreArchivo, string cadena)
+        {
+            if (!File.Exists(nombreArchivo))
+            {
+                throw new ArchivoNoEncontradoException("Archivo no encontrado");
+            }
+            else
+            {
+                try
+                {
+                    using (FileStream fs = new FileStream(nombreArchivo, FileMode.Append, FileAccess.Write))
+                    {
+                        byte[] bytes = Encoding.UTF8.GetBytes("\n" + cadena);
+                        fs.Write(bytes, 0, bytes.Length);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Ocurrió un error: {e.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ EjerciciosModulos/EjerciciosModulo13/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese un Nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese la Edad: ");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la Localidad: ");
+            string localidad = Console.ReadLine();
+
+            string cadena = $"{nombre}|{edad}|{localidad}";
+            string nombreArchivo = $"{nombre}.txt";
+
+            try
+            {
+                FileManager.VerificarArchivo(nombreArchivo);
+                Console.WriteLine("El archivo ya existe. Escoja una opción");
+                PedirDatos(nombreArchivo);
+            }
+            catch(FileManager.ArchivoNoEncontradoException e)
+            {
+                Console.WriteLine(e.Message);
+                try
+                {
+                    FileManager.CrearArchivo(nombreArchivo, cadena);
+                    Console.WriteLine("Archivo creado correctamente.");
+                    PedirDatos(nombreArchivo);
+                }
+                catch(FileManager.ArchivoSiExisteException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static void PedirDatos(string nombreArchivo)
+        {
+            while (true)
+            {
+                Console.WriteLine("Quieres añadir más personas? (S/N):");
+                string opcion = Console.ReadLine();
+
+                if (opcion.ToLower().Equals("s"))
+                {
+                    Console.WriteLine("Ingrese un Nombre: ");
+                    string nombre = Console.ReadLine();
+                    Console.WriteLine("Ingrese la Edad: ");
+                    int edad = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese la Localidad: ");
+                    string localidad = Console.ReadLine();
+
+                    string cadena = $"{nombre}|{edad}|{localidad}";
+
+                    try
+                    {
+                        FileManager.AgregarTexto(nombreArchivo, cadena);
+                    }
+                    catch (FileManager.ArchivoNoEncontradoException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        FileManager.MostrarArchivo(nombreArchivo);
+                    }
+                    catch (FileManager.ArchivoNoEncontradoException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
