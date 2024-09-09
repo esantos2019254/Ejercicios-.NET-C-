13a65a77efb3af1a66aed853aabeef5dac0c80d4
esantos2019254 <esantos-2019254@kinal.edu.gt>
2024-09-09 16:44:58 -0600

    Inicializacion de proyecto de cuentas

---
+++ EjerciciosModulos/Proyecto1/CuentaConPropina/CuentaConPropina/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CuentaConPropina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el total a pagar de su cuenta.");
+            float totalCuenta = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el % de propina que desea agregar a la cuenta.");
+            float porcentaje = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número de personas que participaran en el pago.");
+            int numPersonas = int.Parse(Console.ReadLine());
+
+            float totalPagar = TotalPagar(totalCuenta, porcentaje);
+            float numeroPersona = NumeroPersona(totalPagar, numPersonas);
+
+            Console.WriteLine(numeroPersona);
+
+            Console.Read();
+        }
+        static float TotalPagar(float totalCuenta, float porcentaje)
+        {
+            porcentaje /= 100;
+            float propina = totalCuenta * porcentaje;
+            float total = totalCuenta + propina;
+            return total;
+        }
+        static float NumeroPersona(float total, int numPersonas)
+        {
+            float montoPagarPersona = (float)Math.Round(total / numPersonas, 2);
+            return montoPagarPersona;
+        }
+
+    }
+}
