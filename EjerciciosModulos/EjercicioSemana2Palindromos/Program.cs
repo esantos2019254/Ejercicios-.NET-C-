a44d00e168fcf994678b2065e15ed8a0500218cf
esantos2019254 <esantos-2019254@kinal.edu.gt>
2024-09-17 16:43:05 -0600

    Avance de proyecto 3

---
+++ EjerciciosModulos/EjercicioSemana2Palindromos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace EjercicioSemana2Palindromos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una palabra o frase");
+            string frase = Console.ReadLine().Trim();
+            bool esPalindromo = EsPalindromo(frase);
+            if (esPalindromo == true)
+            {
+                Console.WriteLine("La palabra o frase es PALÍNDROMO.");
+            }
+            else
+            {
+                Console.WriteLine("La palabra o frase no es PALÍNDROMO.");
+            }
+            Console.ReadKey();
+        }
+
+        static bool EsPalindromo(string frase)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach(char c in frase)
+            {
+                if (char.IsLetterOrDigit(c))
+                {
+                    sb.Append(char.ToLower(c));
+                }
+            }
+
+            string cadenaLimpia = sb.ToString();
+            char[] arrayFrase = cadenaLimpia.ToCharArray();
+
+            Array.Reverse(arrayFrase);
+            string fraseReversa = new string(arrayFrase);
+
+            return cadenaLimpia.Equals(fraseReversa);
+        }
+    }
+}
---
+++ EjerciciosModulos/ProyectoSemana3SistemaDeNotas/ProyectoSemana3SistemaDeNotas/Alumno.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSemana3SistemaDeNotas
+{
+    class Alumno
+    {
+        public string Nombre;
+        string Apellido;
+        string NumIdentificacion;
+        string Carrera;
+        List<double> Notas;
+
+        public Alumno()
+        {
+            Notas = new List<double>();
+        }
+
+        public Alumno(string nombre, string apellido, string numIdentificacion, string carrera, List<double> notas)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            NumIdentificacion = numIdentificacion;
+            Carrera = carrera;
+            Notas = notas;
+        }
+        public Alumno(string nombre, string apellido, string numIdentificacion, string carrera, params double[] notas)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            NumIdentificacion = numIdentificacion;
+            Carrera = carrera;
+            Notas = new List<double>(notas);
+        }
+
+        public void setNombre(string nombre)
+        {
+            Nombre = nombre;
+        }
+        public void setApellido(string apellido)
+        {
+            Apellido = apellido;
+        }
+        public void setNumIdentificacion(string numIdentificacion)
+        {
+            NumIdentificacion = numIdentificacion;
+        }
+        public void setCarrera(string carrera)
+        {
+            Carrera = carrera;
+        }
+        public void AgregarNota(double nota)
+        {
+            Notas.Add(nota);
+        }
+        public void ObtenerPromedio()
+        {
+            Notas.Average();
+        }
+    }
+}
