b7eb66260ba8e28921dc3fdce10dc136c6d90a0a
esantos2019254 <esantos-2019254@kinal.edu.gt>
2024-09-19 15:29:26 -0600

    Actualizacion

---
+++ Proyectos/Proyecto3/ProyectoSemana3SistemaDeNotas/Alumno.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProyectoSemana3SistemaDeNotas
+{
+    class Alumno
+    {
+        string Nombre;
+        string Apellido;
+        string NumIdentificacion;
+        string Carrera;
+        List<double> Notas;
+
+        public Alumno()
+        {
+            Notas = new List<double>();
+        }
+
+        public Alumno(string nombre, string apellido, string numIdentificacion, string carrera, List<double> notas)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            NumIdentificacion = numIdentificacion;
+            Carrera = carrera;
+            Notas = notas;
+        }
+        public Alumno(string nombre, string apellido, string numIdentificacion, string carrera, params double[] notas)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            NumIdentificacion = numIdentificacion;
+            Carrera = carrera;
+            Notas = new List<double>(notas);
+        }
+
+        public void setNombre(string nombre)
+        {
+            Nombre = nombre;
+        }
+        public void setApellido(string apellido)
+        {
+            Apellido = apellido;
+        }
+        public void setNumIdentificacion(string numIdentificacion)
+        {
+            NumIdentificacion = numIdentificacion;
+        }
+        public void setCarrera(string carrera)
+        {
+            Carrera = carrera;
+        }
+        public void AgregarNota(double nota)
+        {
+            Notas.Add(nota);
+        }
+        public Double ObtenerPromedio()
+        {
+            return Math.Round(Notas.Average(), 2);
+        }
+
+        public void InfoAlumno()
+        {
+            Console.WriteLine($"No.{NumIdentificacion} Nombre: {Nombre} {Apellido} => Clase: {Carrera} => Promedio: {ObtenerPromedio()}");
+        }
+    }
+}
---
+++ Proyectos/Proyecto3/ProyectoSemana3SistemaDeNotas/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProyectoSemana3SistemaDeNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+            alumno1.setNombre("Juan");
+            alumno1.setApellido("Perez");
+            alumno1.setNumIdentificacion("0001");
+            alumno1.setCarrera("Artes");
+            alumno1.AgregarNota(59);
+            alumno1.AgregarNota(89.45);
+            alumno1.AgregarNota(83.3);
+            alumno1.AgregarNota(43);
+
+            Alumno alumno2 = new Alumno("Elmer", "Santos", "0002", "Informatica", new List<double> { 80, 60.4, 70, 65 });
+
+            Alumno alumno3 = new Alumno("Rodrigo", "García", "0003", "Ciencias", 80, 23.45, 32.34, 79);
+
+            Alumno alumno4 = new Alumno("Fernando", "Perez", "0004", "Tecnologia", 45, 73.45, 38.34, 55);
+
+            Alumno alumno5 = new Alumno("Rodrigo", "García", "0005", "Economia", 95, 12.45, 63.34, 73);
+
+            List<Alumno> alumnos = new List<Alumno> { alumno1, alumno2, alumno3, alumno4, alumno5};
+
+            Console.WriteLine("--------CONTROL ACADÉMICO--------- \n");
+
+            foreach(var datos in alumnos)
+            {
+                datos.InfoAlumno();
+            }
+
+            Console.WriteLine("\nAlumnos con promedio arriba de 60 \n");
+
+            foreach (var datos in alumnos.Where(x => x.ObtenerPromedio() >= 60))
+            {
+                datos.InfoAlumno();
+            }
+
+            double promedioGeneral = Math.Round(alumnos.Average(x => x.ObtenerPromedio()), 2);
+
+            Console.WriteLine($"\nEl promedio general de la clase es de {promedioGeneral}");
+
+            Console.ReadKey();
+        }
+    }
+}
