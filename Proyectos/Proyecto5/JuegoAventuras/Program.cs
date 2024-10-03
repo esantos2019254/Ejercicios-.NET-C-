bb340163ab1b1936182959b2dc407497586a2718
esantos2019254 <esantos-2019254@kinal.edu.gt>
2024-10-03 15:45:23 -0600

    Manual de git subido y proyecto terminado 5

---
+++ Proyectos/Proyecto5/JuegoAventuras/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+
+namespace JuegoAventuras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jugador jugador = null;
+            bool salirDelJuego = false;
+
+            while (!salirDelJuego)
+            {
+                string nombre = "";
+                string archivo = "";
+                Console.WriteLine("Bienvenido, Ingrese un opción.");
+                Console.WriteLine("1. Crear un nuevo jugador.");
+                Console.WriteLine("2. Cargar datos del jugador.");
+                Console.WriteLine("3. Salir");
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        bool nombreValido = false;
+                        do
+                        {
+                            Console.WriteLine("Ingrese su nombre de usario para su jugador.");
+                            nombre = Console.ReadLine().ToLower();
+                            archivo = $"{nombre}.txt";
+
+                            if (File.Exists(archivo))
+                            {
+                                Console.WriteLine("Este nombre ya existe, ingrese otro.");
+                            }
+                            else
+                            {
+                                nombreValido = true;
+                                jugador = new Jugador(nombre);
+                                jugador.Guardar(archivo);
+                                Console.WriteLine("Jugador creado exitosamente!!");
+                                Jugar(jugador, archivo);
+                            }
+                        } while (!nombreValido);
+                        break;
+                    case 2:
+                        Console.WriteLine("Ingrese su nombre");
+                        nombre = Console.ReadLine().ToLower();
+                        archivo = $"{nombre}.txt";
+                        if (!File.Exists(archivo))
+                        {
+                            Console.WriteLine("Este jugador no existe.");
+                        }
+                        else
+                        {
+                            jugador = new Jugador();
+                            Jugador j = jugador.Cargar(archivo);
+                            j.InformacionJugador();
+                            Jugar(j, archivo);
+                        }
+                        break;
+                    case 3:
+                        salirDelJuego = true;
+                        Console.WriteLine("Saliendo....");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+        static void Jugar(Jugador jugador, string archivo)
+        {
+            bool salirDelSubmenu = false;
+            do
+            {
+                Random random = new Random();
+                Console.WriteLine("¿Qué quieres hacer?");
+                Console.WriteLine("1. Jugar");
+                Console.WriteLine("2. Guardar progreso.");
+                Console.WriteLine("3. Regresar.");
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        bool menu = false;
+                        jugador.InformacionJugador();
+                        do
+                        {
+                            Console.WriteLine("Ingresa el numero en pantalla si fallas pierdes. Suerte!!");
+                            int numeroCorrecto = random.Next(1, 100);
+                            Console.WriteLine($"Numero a ingresar: {numeroCorrecto}");
+                            int numeroIngresado = int.Parse(Console.ReadLine());
+
+                            if (numeroCorrecto == numeroIngresado)
+                            {
+                                jugador.GanarBatalla();
+                                Console.WriteLine($"Has pasado al nivel: {jugador.Nivel}");
+                                menu = true;
+                            }
+                            else
+                            {
+                                jugador.PerderBatalla();
+                                if (jugador.Vidas == 0)
+                                {
+                                    jugador.Reiniciar();
+                                    menu = true;
+                                }
+                            }
+                        } while (!menu);
+                        break;
+                    case 2:
+                        jugador.InformacionJugador();
+                        jugador.Guardar(archivo);
+                        break;
+                    case 3:
+                        salirDelSubmenu = true;
+                        Console.WriteLine("Regresando al inicio....");
+                        break;
+                    default:
+                        break;
+                }
+            } while (!salirDelSubmenu);
+        }
+    }
+}
---
+++ Proyectos/Proyecto5/Jugador.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace JuegoAventuras
+{
+    [Serializable]
+    class Jugador
+    {
+        public string Nombre;
+        public int Nivel;
+        public int Vidas;
+        public int Muertes;
+        public Jugador()
+        {
+
+        }
+        public Jugador (string nombre)
+        {
+            Nombre = nombre;
+            Nivel = 1;
+            Vidas = 3;
+            Muertes = 0;
+        }
+
+        public void InformacionJugador()
+        {
+            Console.WriteLine($"\nNombre: {Nombre} Nivel: {Nivel} Vidas: {Vidas} Muertes: {Muertes}\n");
+        }
+
+        public void GanarBatalla()
+        {
+            Console.WriteLine("Has ganado la batalla!!!!");
+            SubirNivel();
+        }
+
+        public void SubirNivel()
+        {
+            Nivel++;
+            Vidas = 3;
+            Muertes = 0;
+        }
+
+        public void PerderBatalla()
+        {
+            Console.WriteLine("\nHas perdido la batalla :(");
+            Vidas--;
+            Muertes++;
+            if(Vidas == 0)
+            {
+                Console.WriteLine("Fin del juego. Intentalo de nuevo.\n");
+            }else
+            {
+                Console.WriteLine($"\nTe Quedan {Vidas} Vidas.  Muertes: {Muertes}\n");
+            }
+        }
+        public void Reiniciar()
+        {
+            Vidas = 3;
+            Muertes = 0;
+        }
+        public void Guardar(string rutaArchivo)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(rutaArchivo, FileMode.Create))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(fs, this);
+                    Console.WriteLine("Datos del jugador guardados correctamente.");
+                }
+            }
+            catch (Exception e)
+            {
+                RegistrarError(e);
+                Console.WriteLine("Ocurrio un error al guardar los datos.");
+            }
+        }
+
+        public Jugador Cargar (String rutaArchivo)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(rutaArchivo, FileMode.Open))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    Jugador jugador = (Jugador)formatter.Deserialize(fs);
+                    return jugador;
+                }
+            }
+            catch (Exception e)
+            {
+                RegistrarError(e);
+                Console.WriteLine("Ocurrio un error al cargar los datos.");
+                return null;
+            }
+        }
+
+        public static void RegistrarError(Exception e)
+        {
+            string rutaLog = "logErrores.txt";
+
+            using(StreamWriter sw = new StreamWriter(rutaLog, true))
+            {
+                sw.WriteLine($"{DateTime.Now}: {e.Message}");
+            }
+        }
+    }
+}
